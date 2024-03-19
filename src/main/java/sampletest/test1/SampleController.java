abda2f9087e29a47742f033155aea468cf6f9bce
Ashokku5 <159868139+Ashokku5@users.noreply.github.com>
2024-03-19 09:59:56 +0530

    Add files via upload

---
+++ src/main/java/sampletest/test1/SampleController.java
@@ -0,0 +1,13 @@
+package sampletest.test1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+	@RequestMapping("sample")
+	public String helloSpring(){
+		return "Hello Spring";
+	}
+}
